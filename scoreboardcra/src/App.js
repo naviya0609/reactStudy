1a187081a89cd936334001482bf01a2075489bf6
김주희 <naviya0609@gmail.com>
2019-10-26 17:46:39 +0900

    foreach / map을 이용해 배열 setstate

--- scoreboardcra/src/App.js
+++ scoreboardcra/src/App.js
@@ -24,13 +24,21 @@ class App extends React.Component {
     //console.log("handleChangeScore", id, delta)
     this.setState(prevState => {
         //새로운배열로 copy 기존바구니에서 펼쳐서 새로운 배열 만듦.
-        const players = [ ...prevState.players ]
-        players.forEach(player => {
+        /*const players = [ ...prevState.players ]
+        players.forEach(player => { //forEach는 리턴없이 해당 배열로 리턴.
           if(id === player.id){
             player.score += delta;
           }
         })
-      return {players};
+        return {players}; //  {players : players};하나로 생략*/
+
+      const players = prevState.players.map(player=>{ //리턴되는 것을 새로운 배열로 만들어서 리턴.
+          if(id === player.id){         //player를 각각담아서 배열로 리턴
+            player.score += delta;
+          }
+          return player
+        })
+        return { players };
     })
   }
   render() {
