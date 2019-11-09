4a02f01b7de5c28540f31ff21ffbc7611f8bc1bf
김주희 <rlawngml28@gmail.com>
2019-11-09 17:26:07 +0900

    async / await / promise data get

---
+++ scoreboardcra/src/pages/heroes/Heroes.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import axios from 'axios';
+import './Heroes.scss';
+
+export class Heroes extends React.Component {
+	state = {
+		heroes: []
+	}
+
+	componentDidMount() {
+		this.getHeroes();
+	}
+
+	//async 는 비동기가 되고, 반드시 await 와 같이 사용함.
+	//await 는 반드시 뒤에 promise가 오고 , promise가 올때까지 기다린다.
+	getHeroes = async () => {
+		let response = await axios.get('http://ec2-15-164-134-124.ap-northeast-2.compute.amazonaws.com:8000/api/user/heroes?start_index=0&page_size=10 ');
+		console.log(response);//response 비동기 PROMISE RETURN
+		this.setState({
+			heroes: response.data.data
+		});
+	}
+
+	render() {
+		return (
+			<ul className="img-box">
+				{this.state.heroes.map(hero => (
+					<li key={hero.id} className="row align-items-center m-0">
+						<div className="col-1 py-2">
+							<img src={hero.photo} alt={hero.name} className="img-fluid rounded-circle" />
+						</div>
+						<span className="col">{hero.name}</span>
+					</li>
+				))}
+			</ul>
+		);
+	}
+}
